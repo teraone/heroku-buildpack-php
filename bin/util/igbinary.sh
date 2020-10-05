73412cd32465b44bec6f304032785e3a35f6a0ce
Patrick Kriechbaumer <pk@pk-informatics.at>
2020-10-05 15:49:02 +0200

    update igbinary stuff

--- bin/util/igbinary.sh
+++ bin/util/igbinary.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-	if [[ "$engine" == "php" && -n "$NEW_RELIC_LICENSE_KEY" ]] && ! $engine -n $(which composer) show -d "$build_dir/.heroku/php" --installed --quiet heroku-sys/ext-newrelic 2>/dev/null; then
+	if [[ "$engine" == "php" ]] && ! $engine -n $(which composer) show -d "$build_dir/.heroku/php" --installed --quiet heroku-sys/ext-igbinary 2>/dev/null; then
 		if $engine -n $(which composer) require --update-no-dev -d "$build_dir/.heroku/php" -- "heroku-sys/ext-igbinary:*" >> $build_dir/.heroku/php/install.log 2>&1; then
 			echo "- New Relic detected, installed ext-igbinary" | indent
 		else
